d6824afd891846a0d0d9d3744029d1ca38626638
akshayrb22 <akshay.bhat981222@gmail.com>
2019-10-29 21:33:30 +0530

    Final student details ready. Need to display aggregated data.

--- src/students.js
+++ src/students.js
@@ -32,30 +32,13 @@ import {
 } from "react-admin";
 import RichTextInput from "ra-input-rich-text";
 
-const ExpandSubjectsPanel = props => (
-  <Show {...props}>
-    <TabbedShowLayout>
-      <Tab label="Subject 1">
-        <TextField source="subject1[1]" label="Subject 1 Code" />
-        <TextField source="subject1[2]" label="CIE 1" />
-        <TextField source="subject1[3]" label="CIE 2" />
-        <TextField source="subject1[4]" label="CIE 3" />
-        <TextField source="subject1[5]" label="Final" />
-      </Tab>
-      <Tab label="Subject 2">
-        <TextField source="subject2[1]" label="Subject 2 Code" />
-        <TextField source="subject2[2]" label="CIE 1" />
-        <TextField source="subject2[3]" label="CIE 2" />
-        <TextField source="subject2[4]" label="CIE 3" />
-        <TextField source="subject2[5]" label="Final" />
-      </Tab>
-    </TabbedShowLayout>
-  </Show>
-);
+// const ExpandSubjectsPanel = props => (
+
+// );
 
 const StudentList = props => (
   <List {...props}>
-    <Datagrid rowClick="edit" expand={<ExpandSubjectsPanel />}>
+    <Datagrid rowClick="edit">
       <TextField source="batch" />
       <TextField source="name" />
       <TextField source="usn" label="USN" />
@@ -65,6 +48,7 @@ const StudentList = props => (
       <TextField source="subject4[0]" label="Subject 4" />
       <TextField source="subject5[0]" label="Subject 5" />
       <EditButton />
+      <ShowButton />
     </Datagrid>
   </List>
 );
@@ -113,87 +97,45 @@ const StudentCreate = props => (
 const StudentShow = props => (
   <Show {...props}>
     <TabbedShowLayout>
-      <Tab label="Student Info">
-        <TextField source="admission number" />
-        <TextField source="name" />
-        <TextField source="gender" />
-        <TextField source="class" />
-        <TextField source="department" />
-        <TextField source="Location" />
-        <TextField source="academicYear" />
-        <TextField source="address" />
-        <TextField source="city" />
-        <TextField source="country" />
-        <TextField source="dateOfJoining" />
-        <TextField source="dob" label="Date of Birth" />
-        <TextField source="pincode" />
-        <TextField source="profilePhoto" />
+      <Tab label="Subject 1">
+        <TextField source="subject1[0]" label=""/>
+        <TextField source="subject1[1]" label="Subject 1 Code" />
+        <NumberField source="subject1[2]" label="CIE 1" />
+        <NumberField source="subject1[3]" label="CIE 2" />
+        <NumberField source="subject1[4]" label="CIE 3" />
+        <NumberField source="subject1[5]" label="Final" />
       </Tab>
-
-      <Tab label="Parent Info">
-        <ReferenceManyField
-          label="Parent"
-          reference="parents"
-          target="admission number"
-        >
-          <SingleFieldList>
-            <TextField source="pname" />
-          </SingleFieldList>
-        </ReferenceManyField>
-        <ReferenceManyField
-          label="Profession"
-          reference="parents"
-          target="admission number"
-        >
-          <SingleFieldList>
-            <TextField source="profession" />
-          </SingleFieldList>
-        </ReferenceManyField>
-        <ReferenceManyField
-          label="Email"
-          reference="parents"
-          target="admission number"
-        >
-          <SingleFieldList>
-            <TextField source="email" />
-          </SingleFieldList>
-        </ReferenceManyField>
-        <ReferenceManyField
-          label="Phone"
-          reference="parents"
-          target="admission number"
-        >
-          <SingleFieldList>
-            <TextField source="phone" />
-          </SingleFieldList>
-        </ReferenceManyField>
-        <ReferenceManyField
-          label="Relationship"
-          reference="parents"
-          target="admission number"
-        >
-          <SingleFieldList>
-            <TextField source="relationship" />
-          </SingleFieldList>
-        </ReferenceManyField>
-        <ReferenceManyField
-          label="Date of Birth"
-          reference="parents"
-          target="admission number"
-        >
-          <SingleFieldList>
-            <TextField source="dob" />
-          </SingleFieldList>
-        </ReferenceManyField>
-        <ReferenceManyField
-          label="Blood Group"
-          reference="parents"
-          target="admission number"
-        >
-          <SingleFieldList>
-            <TextField source="blood group" />
-          </SingleFieldList>
-        </ReferenceManyField>
+      <Tab label="Subject 2">
+        <TextField source="subject2[0]" label=""/>
+        <TextField source="subject2[1]" label="Subject 2 Code" />
+        <NumberField source="subject2[2]" label="CIE 1" />
+        <NumberField source="subject2[3]" label="CIE 2" />
+        <NumberField source="subject2[4]" label="CIE 3" />
+        <NumberField source="subject2[5]" label="Final" />
+      </Tab>
+      <Tab label="Subject 3">
+        <TextField source="subject3[0]" label=""/>
+        <TextField source="subject3[1]" label="Subject 3 Code" />
+        <NumberField source="subject3[2]" label="CIE 1" />
+        <NumberField source="subject3[3]" label="CIE 2" />
+        <NumberField source="subject3[4]" label="CIE 3" />
+        <NumberField source="subject3[5]" label="Final" />
+      </Tab>
+      <Tab label="Subject 4">
+        <TextField source="subject4[0]" label=""/>
+        <TextField source="subject4[1]" label="Subject 4 Code" />
+        <NumberField source="subject4[2]" label="CIE 1" />
+        <NumberField source="subject4[3]" label="CIE 2" />
+        <NumberField source="subject4[4]" label="CIE 3" />
+        <NumberField source="subject4[5]" label="Final" />
+      </Tab>
+      <Tab label="Subject 5">
+        <TextField source="subject5[0]" label=""/>
+        <TextField source="subject5[1]" label="Subject 5 Code" />
+        <NumberField source="subject5[2]" label="CIE 1" />
+        <NumberField source="subject5[3]" label="CIE 2" />
+        <NumberField source="subject5[4]" label="CIE 3" />
+        <NumberField source="subject5[5]" label="Final" />
       </Tab>
     </TabbedShowLayout>
   </Show>
