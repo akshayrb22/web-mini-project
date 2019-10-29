636ee75896a4a7fc38d654371e7b3f200f70e0c9
akshayrb22 <akshay.bhat981222@gmail.com>
2019-10-29 23:18:48 +0530

    Student data entry capabilities added. TODO: Enter some dummy values and create a script to aggregate data.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import * as React from "react";
 import {
   StudentList,
-  StudentCreate,
   StudentShow,
-  StudentEdit
+  StudentEdit,
+  StudentCreate
 } from "./students";
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
 // import { StaffList, StaffEdit, StaffCreate,StaffShow} from './staff';
 // import { ParentList, ParentEdit, ParentCreate, ParentShow } from './parents';
 import {
@@ -32,7 +32,13 @@ class App extends React.Component {
         dataProvider={dataProvider}
         authProvider={authProvider}
       >
-        <Resource name="students" list={StudentList} show={StudentShow} />
+        <Resource
+          name="students"
+          list={StudentList}
+          show={StudentShow}
+          edit={StudentEdit}
+          create={StudentCreate}
+        />
       </Admin>
     );
   }
--- src/students.js
+++ src/students.js
@@ -7,34 +7,19 @@ import {
   Show,
   Create,
   Edit,
-  Filter,
   Tab,
-  DisabledInput,
-  SimpleShowLayout,
   TabbedShowLayout,
-  SimpleForm,
   TextField,
   TextInput,
   ShowButton,
   EditButton,
-  DeleteButton,
-  RichTextField,
-  SelectInput,
-  DateInput,
+  NumberInput,
   NumberField,
-  DateField,
-  EmailField,
-  ReferenceManyField,
-  SingleFieldList,
-  ChipField,
-  ReferenceInput,
-  AutocompleteInput
+  TabbedForm,
+  FormTab,
+  SelectInput
 } from "react-admin";
-import RichTextInput from "ra-input-rich-text";
 
-// const ExpandSubjectsPanel = props => (
-
-// );
 
 const StudentList = props => (
   <List {...props}>
@@ -55,50 +40,127 @@ const StudentList = props => (
 
 const StudentEdit = props => (
   <Edit {...props}>
-    <SimpleForm>
-      <DisabledInput source="id" />
-      <TextInput source="academic year" />
-      <TextInput source="admission number" />
-      <TextInput source="address" />
-      <TextInput source="city" />
-      <TextInput source="class" />
-      <TextInput source="country" />
-      <TextInput source="department" />
-      <DateInput source="dob" />
-      <TextInput source="doj" />
-      <TextInput source="gender" />
-      <TextInput source="location" />
-      <TextInput source="name" />
-      <TextInput source="phone" />
-      <TextInput source="pincode" />
-    </SimpleForm>
+    <TabbedForm>
+      <FormTab label="Personal">
+        <SelectInput
+          source="batch"
+          choices={[
+            { id: "16-20", name: "2016 - 2020" },
+            { id: "17-21", name: "2017 - 2021" },
+            { id: "18-22", name: "2018 - 2022" },
+            { id: "19-23", name: "2019 - 2023" }
+          ]}
+        />
+        <TextInput source="name" />
+        <TextInput source="usn" label="USN" />
+      </FormTab>
+      <FormTab label="Subject 1">
+        <TextInput source="subject1[0]" label="" />
+        <TextInput source="subject1[1]" label="Subject 1 Code" />
+        <NumberInput source="subject1[2]" label="CIE 1" />
+        <NumberInput source="subject1[3]" label="CIE 2" />
+        <NumberInput source="subject1[4]" label="CIE 3" />
+        <NumberInput source="subject1[5]" label="Final" />
+      </FormTab>
+      <FormTab label="Subject 2">
+        <TextField source="subject2[0]" label="" />
+        <TextField source="subject2[1]" label="Subject 2 Code" />
+        <NumberInput source="subject2[2]" label="CIE 1" />
+        <NumberInput source="subject2[3]" label="CIE 2" />
+        <NumberInput source="subject2[4]" label="CIE 3" />
+        <NumberInput source="subject2[5]" label="Final" />
+      </FormTab>
+      <FormTab label="Subject 3">
+        <TextField source="subject3[0]" label="" />
+        <TextField source="subject3[1]" label="Subject 3 Code" />
+        <NumberInput source="subject3[2]" label="CIE 1" />
+        <NumberInput source="subject3[3]" label="CIE 2" />
+        <NumberInput source="subject3[4]" label="CIE 3" />
+        <NumberInput source="subject3[5]" label="Final" />
+      </FormTab>
+      <FormTab label="Subject 4">
+        <TextField source="subject4[0]" label="" />
+        <TextField source="subject4[1]" label="Subject 4 Code" />
+        <NumberInput source="subject4[2]" label="CIE 1" />
+        <NumberInput source="subject4[3]" label="CIE 2" />
+        <NumberInput source="subject4[4]" label="CIE 3" />
+        <NumberInput source="subject4[5]" label="Final" />
+      </FormTab>
+      <FormTab label="Subject 5">
+        <TextField source="subject5[0]" label="" />
+        <TextField source="subject5[1]" label="Subject 5 Code" />
+        <NumberInput source="subject5[2]" label="CIE 1" />
+        <NumberInput source="subject5[3]" label="CIE 2" />
+        <NumberInput source="subject5[4]" label="CIE 3" />
+        <NumberInput source="subject5[5]" label="Final" />
+      </FormTab>
+    </TabbedForm>
   </Edit>
 );
 const StudentCreate = props => (
   <Create {...props}>
-    <SimpleForm>
-      <TextInput source="admission number" />
-      <TextInput source="Location" />
-      <DateInput source="academicYear" />
-      <TextInput source="address" />
-      <TextInput source="city" />
-      <TextInput source="class" />
-      <TextInput source="country" />
-      <DateInput source="dateOfJoining" />
-      <TextInput source="department" />
-      <TextInput source="dob" />
-      <TextInput source="gender" />
-      <TextInput source="name" />
-      <TextInput source="pincode" />
-      <TextInput source="profilePhoto" />
-    </SimpleForm>
+    <TabbedForm>
+      <FormTab label="Personal">
+        <SelectInput
+          source="batch"
+          choices={[
+            { id: "16-20", name: "2016 - 2020" },
+            { id: "17-21", name: "2017 - 2021" },
+            { id: "18-22", name: "2018 - 2022" },
+            { id: "19-23", name: "2019 - 2023" }
+          ]}
+        />
+        <TextInput source="name" />
+        <TextInput source="usn" label="USN" />
+      </FormTab>
+      <FormTab label="Subject 1">
+        <TextInput source="subject1[0]" label="" />
+        <TextInput source="subject1[1]" label="Subject 1 Code" />
+        <NumberInput source="subject1[2]" label="CIE 1" />
+        <NumberInput source="subject1[3]" label="CIE 2" />
+        <NumberInput source="subject1[4]" label="CIE 3" />
+        <NumberInput source="subject1[5]" label="Final" />
+      </FormTab>
+      <FormTab label="Subject 2">
+        <TextField source="subject2[0]" label="" />
+        <TextField source="subject2[1]" label="Subject 2 Code" />
+        <NumberInput source="subject2[2]" label="CIE 1" />
+        <NumberInput source="subject2[3]" label="CIE 2" />
+        <NumberInput source="subject2[4]" label="CIE 3" />
+        <NumberInput source="subject2[5]" label="Final" />
+      </FormTab>
+      <FormTab label="Subject 3">
+        <TextField source="subject3[0]" label="" />
+        <TextField source="subject3[1]" label="Subject 3 Code" />
+        <NumberInput source="subject3[2]" label="CIE 1" />
+        <NumberInput source="subject3[3]" label="CIE 2" />
+        <NumberInput source="subject3[4]" label="CIE 3" />
+        <NumberInput source="subject3[5]" label="Final" />
+      </FormTab>
+      <FormTab label="Subject 4">
+        <TextField source="subject4[0]" label="" />
+        <TextField source="subject4[1]" label="Subject 4 Code" />
+        <NumberInput source="subject4[2]" label="CIE 1" />
+        <NumberInput source="subject4[3]" label="CIE 2" />
+        <NumberInput source="subject4[4]" label="CIE 3" />
+        <NumberInput source="subject4[5]" label="Final" />
+      </FormTab>
+      <FormTab label="Subject 5">
+        <TextField source="subject5[0]" label="" />
+        <TextField source="subject5[1]" label="Subject 5 Code" />
+        <NumberInput source="subject5[2]" label="CIE 1" />
+        <NumberInput source="subject5[3]" label="CIE 2" />
+        <NumberInput source="subject5[4]" label="CIE 3" />
+        <NumberInput source="subject5[5]" label="Final" />
+      </FormTab>
+    </TabbedForm>
   </Create>
 );
 const StudentShow = props => (
   <Show {...props}>
     <TabbedShowLayout>
       <Tab label="Subject 1">
-        <TextField source="subject1[0]" label=""/>
+        <TextField source="subject1[0]" label="" />
         <TextField source="subject1[1]" label="Subject 1 Code" />
         <NumberField source="subject1[2]" label="CIE 1" />
         <NumberField source="subject1[3]" label="CIE 2" />
@@ -106,7 +168,7 @@ const StudentShow = props => (
         <NumberField source="subject1[5]" label="Final" />
       </Tab>
       <Tab label="Subject 2">
-        <TextField source="subject2[0]" label=""/>
+        <TextField source="subject2[0]" label="" />
         <TextField source="subject2[1]" label="Subject 2 Code" />
         <NumberField source="subject2[2]" label="CIE 1" />
         <NumberField source="subject2[3]" label="CIE 2" />
@@ -114,7 +176,7 @@ const StudentShow = props => (
         <NumberField source="subject2[5]" label="Final" />
       </Tab>
       <Tab label="Subject 3">
-        <TextField source="subject3[0]" label=""/>
+        <TextField source="subject3[0]" label="" />
         <TextField source="subject3[1]" label="Subject 3 Code" />
         <NumberField source="subject3[2]" label="CIE 1" />
         <NumberField source="subject3[3]" label="CIE 2" />
@@ -122,7 +184,7 @@ const StudentShow = props => (
         <NumberField source="subject3[5]" label="Final" />
       </Tab>
       <Tab label="Subject 4">
-        <TextField source="subject4[0]" label=""/>
+        <TextField source="subject4[0]" label="" />
         <TextField source="subject4[1]" label="Subject 4 Code" />
         <NumberField source="subject4[2]" label="CIE 1" />
         <NumberField source="subject4[3]" label="CIE 2" />
@@ -130,7 +192,7 @@ const StudentShow = props => (
         <NumberField source="subject4[5]" label="Final" />
       </Tab>
       <Tab label="Subject 5">
-        <TextField source="subject5[0]" label=""/>
+        <TextField source="subject5[0]" label="" />
         <TextField source="subject5[1]" label="Subject 5 Code" />
         <NumberField source="subject5[2]" label="CIE 1" />
         <NumberField source="subject5[3]" label="CIE 2" />
