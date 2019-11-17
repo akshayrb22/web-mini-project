4f9147592eeabaeadbbd6459bdda0cb57fb9a36e
akshayrb22 <akshay.bhat981222@gmail.com>
2019-11-17 10:22:40 +0530

    Subject stats are being set in Firestore. Need to write query for bands and display them in the frontend.

--- functions/index.js
+++ functions/index.js
@@ -27,7 +27,6 @@ exports.updateSubjectCount = functions.firestore
 			// Deleting document : subtract one from count
 			countRef.update({ count: decrement });
 		}
-
 		return;
 	});
 exports.updateIndividualSubjectStats = functions.firestore
@@ -41,7 +40,7 @@ exports.updateIndividualSubjectStats = functions.firestore
 			.then(function(querySnapshot) {
 				querySnapshot.forEach(function(doc) {
 					// doc.data() is never undefined for query doc snapshots
-					console.log(doc.data());
+
 					for (let i = 0; i < 5; i++) {
 						subject_cie_list[i].push(
 							cie_average(
@@ -57,10 +56,11 @@ exports.updateIndividualSubjectStats = functions.firestore
 				let num_subjects = 5;
 				for (let i = 0; i < num_subjects; i++) {
 					for (let j = 0; j < subject_cie_list[0].length; j++) {
-						final_list[i][j] = parseInt(subject_cie_list[i][j]) + parseInt(sem_list[i][j]);
+						final_list[i][j] =
+							parseInt(subject_cie_list[i][j]) + parseInt(sem_list[i][j]);
 					}
 				}
-				
+
 				let pass_rate = [0, 0, 0, 0, 0];
 				let pass_count = [0, 0, 0, 0, 0];
 				for (let i = 0; i < 5; i++) {
@@ -73,18 +73,62 @@ exports.updateIndividualSubjectStats = functions.firestore
 				for (let i = 0; i < pass_rate.length; i++) {
 					pass_rate[i] = ((pass_count[i] / num_students) * 100).toFixed(2);
 				}
-				console.log(
-					"Num Students: " +
-						num_students +
-						"\n\nPass Rate:" +
-						pass_rate +
-						"\n\nSubject CIE List:" +
-						subject_cie_list +
-						"\n\nSemester list:" +
-						sem_list +
-						"\n\nFinal Marks List:" +
-						final_list
+				// console.log(
+				// 	"Num Students: " +
+				// 		num_students +
+				// 		"\n\nPass Rate:" +
+				// 		pass_rate +
+				// 		"\n\nSubject CIE List:" +
+				// 		subject_cie_list +
+				// 		"\n\nSemester list:" +
+				// 		sem_list +
+				// 		"\n\nFinal Marks List:" +
+				// 		final_list
+				// );
+				let overall_stats = compute_subject_stats(
+					final_list,
+					sem_list,
+					subject_cie_list
 				);
+				let cie_stats = overall_stats[0];
+				let sem_stats = overall_stats[1];
+				let final_stats = overall_stats[2];
+
+				let cie_min = cie_stats[0];
+				let cie_max = cie_stats[1];
+				let cie_avg = cie_stats[2];
+
+				let sem_min = sem_stats[0];
+				let sem_max = sem_stats[1];
+				let sem_avg = sem_stats[2];
+
+				let final_min = final_stats[0];
+				let final_max = final_stats[1];
+				let final_avg = final_stats[2];
+
+				for (let i = 0; i < 5; i++) {
+					let data = {
+						cie_min: cie_min[i],
+						cie_max: cie_max[i],
+						cie_avg: cie_avg[i],
+
+						sem_min: sem_min[i],
+						sem_max: sem_max[i],
+						sem_avg: sem_avg[i],
+
+						final_min: final_min[i],
+						final_max: final_max[i],
+						final_avg: final_avg[i],
+
+						pass_rate: parseInt(pass_rate[i])
+					};
+
+					let set_doc = db
+						.collection("subject-stats")
+						.doc("subject" + (i + 1).toString())
+						.set(data);
+				}
+
 				return [pass_rate, final_list, sem_list, subject_cie_list];
 			})
 			.catch(function(error) {
@@ -93,4 +137,54 @@ exports.updateIndividualSubjectStats = functions.firestore
 	});
 function cie_average(cie1, cie2, cie3) {
 	return (cie1 + cie2 + cie3) / 3;
+}
+function compute_subject_stats(final_list, sem_list, subject_cie_list) {
+	final_max = [];
+	final_min = [];
+	final_avg = [];
+
+	sem_max = [];
+	sem_min = [];
+	sem_avg = [];
+
+	cie_max = [];
+	cie_min = [];
+	cie_avg = [];
+	// subject_cie_list = subject_cie_list.map(Number);
+	sum = 0;
+	for (let i = 0; i < final_list.length; i++) {
+		final_min[i] = Math.min(...final_list[i]);
+		sem_min[i] = Math.min(...sem_list[i]);
+		cie_min[i] = Math.min(...subject_cie_list[i]);
+
+		final_max[i] = Math.max(...final_list[i]);
+		sem_max[i] = Math.max(...sem_list[i]);
+		cie_max[i] = Math.max(...subject_cie_list[i]);
+
+		sum = 0;
+		sum = final_list[i].reduce((previous, current) => (current += previous));
+		final_avg[i] = (sum / final_list[i].length).toFixed(2);
+
+		sum = 0;
+		sum = sem_list[i].reduce((previous, current) => (current += previous));
+		sem_avg[i] = (sum / final_list[i].length).toFixed(2);
+
+		sum = 0;
+		console.log(
+			"Subject " + (i + 1).toString() + " CIE List: " + 	subject_cie_list[i]
+		);
+		sum = subject_cie_list[i].reduce(
+			(previous, current) => (current = Number(current) + Number(previous))
+		);
+		console.log("Subject " + (i + 1).toString() + " sum: " + sum);
+		cie_avg[i] = (sum / final_list[i].length).toFixed(2);
+	}
+	final_avg = final_avg.map(Number);
+	sem_avg = sem_avg.map(Number);
+	cie_avg = cie_avg.map(Number);
+	final_stats = [final_min, final_max, final_avg];
+	sem_stats = [sem_min, sem_max, sem_avg];
+	cie_stats = [cie_min, cie_max, cie_avg];
+	overall_stats = [cie_stats, sem_stats, final_stats];
+	return overall_stats;
 }
